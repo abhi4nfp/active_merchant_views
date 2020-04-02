01603af74fbddaf5103ea2cafed3ea0748d479c3
Abhilash Reddy <abhilashmv@outlook.com>
2020-04-02 16:58:09 +0530

    Recent changes..

--- lib/active_merchant_views_helper.rb
+++ lib/active_merchant_views_helper.rb
@@ -1,5 +1,5 @@
 module ActiveMerchantViewsHelper
-  def render_am_views(payment_gateway:, meta: {})
-    render partial: "active_merchant/#{payment_gateway.to_s}", meta: meta
+  def render_am_views(payment_gateway:, locals: {})
+    render partial: "active_merchant/#{payment_gateway.to_s}", locals: locals
   end
 end
